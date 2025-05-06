3e06b96fb84270b88c85462e9e92aaa861ff8bc3
Ryan Serra <ryan_serra@hotmail.com>
2025-05-06 13:33:17 -0400

    Add debug version of example plugin for testing plugin loading
    Introduced a minimal DebugExamplePlugin class and its supporting manifest file. Adjusted versioning, author metadata, and entry points to differentiate the debug version from the main plugin. This provides a lightweight framework for testing and diagnosing plugin system enhancements.

--- qorzen/plugins/example_plugin/__init__.py
+++ qorzen/plugins/example_plugin/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 from qorzen.plugins.example_plugin.code.plugin import ExamplePlugin
 
-__version__ = "0.2.0"
-__author__ = "Ryan Serra"
+__version__ = "1.0.0"
+__author__ = "Qorzen Team"
 
-__all__ = ["__version__", "__author__", "ExamplePlugin"]
+__all__ = ["ExamplePlugin", "__version__", "__author__"]
